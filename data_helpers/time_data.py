0169b480b615a071a9eab10e76e86a38b9d3ee63
Sam Bland <sbland.co.uk@gmail.com>
2021-05-28 16:50:52 +0100

    Add get julian day function

--- data_helpers/time_data.py
+++ data_helpers/time_data.py
@@ -1,7 +1,14 @@
 """Generic helpers related to time data"""
 
+from datetime import date
+
 
 def get_row_index(day: int, hour: int) -> int:
     """Gets the row index based on day and hour assuming row per hour"""
     row_index: int = (day * 24) + hour
     return row_index
+
+
+def get_julian_day(dt: date) -> int:
+    tt = dt.timetuple()
+    return tt.tm_yday
