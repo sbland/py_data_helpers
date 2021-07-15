d4bf81057b6f19ac5b88c4e6ba8d2bf48ea606c2
Sam Bland <sbland.co.uk@gmail.com>
2021-07-15 17:13:33 +0100

    Add class construction module

---
+++ data_helpers/cls_construction.py
@@ -0,0 +1,212 @@
+"""Various functions for generating classes from lower level data structures such as json."""
+from dataclasses import asdict, dataclass, field, make_dataclass
+from enum import Enum
+from typing import List, Tuple
+
+
+@dataclass
+class FieldBase:
+
+    variable: str
+    label: str
+    cls: type
+    desc: str = ''
+    required: bool = False
+    default: any = None
+    units: any = None
+
+    def __asdict__(self):
+        return asdict(self)
+
+
+@dataclass
+class Group:
+
+    variable: str
+    label: str
+    fields: List[FieldBase]
+
+
+@dataclass
+class Field(FieldBase):
+    dependencies: List[str] = field(default_factory=lambda: [])
+
+
+@dataclass
+class NumberField(FieldBase):
+    dependencies: List[str] = field(default_factory=lambda: [])
+    min: float = 0
+    max: float = 100
+    step: float = 1.0
+
+
+@dataclass
+class Option:
+
+    uid: str
+    label: str
+    dependencies: List[str]  # parameters required by option
+
+
+@dataclass
+class Select(FieldBase):
+
+    options: List[Tuple[str, str]] = field(default_factory=lambda: [])
+    required_params: List[str] = field(default_factory=lambda: [])
+
+
+def generate_enum_from_select(f: Select):
+    return Enum(f"{f.variable.capitalize()}Enum", dict([(opt.label, opt.uid) for opt in f.options]))
+
+
+def field_to_dataclass_field(f: FieldBase, module):
+    field_out = None
+    Cls = None
+    if isinstance(f, Group):
+        Cls, subclasses = group_to_class(f.label, f, module)
+        field_out = (f.variable, Cls)
+    elif isinstance(f, Field):
+        Cls = f.cls
+        if f.default:
+            field_out = (f.variable, Cls, field(default=f.default))
+        else:
+            field_out = (f.variable, Cls)
+    elif isinstance(f, NumberField):
+        Cls = f.cls
+        if f.default:
+            field_out = (f.variable, Cls, field(default=f.default))
+        else:
+            field_out = (f.variable, Cls)
+    elif isinstance(f, Select):
+        Cls = generate_enum_from_select(f)
+        field_out = (f.variable, Cls)
+    else:
+        raise ValueError(f"Invalid fieldtype: {type(f)}")
+    return field_out, (f.label, Cls)
+
+
+def sort_fields(field: Field) -> bool:
+    """Sort dataclass construction fields.
+
+    Makes fields with default arg occur after fields without default arg.
+    To avoid error: `non-default argument 'sel' follows default argument`
+    """
+    return 1 if getattr(field, 'default', None) is not None else 0
+
+
+def group_to_class(cls_name, group: Group, module) -> object:
+    subclasses = {}
+    fields_parsed, classes = zip(*[field_to_dataclass_field(f, module)
+                                   for f in sorted(group.fields, key=sort_fields)])
+    subclasses = {
+        **subclasses,
+        **dict(classes),
+    }
+    obj = make_dataclass(cls_name, fields_parsed)
+    obj.__doc__ = "Generated Dataclass"
+    obj.__module__ = module
+    return obj, subclasses
+
+
+class Struct(object):
+    """Create a class from a dictionary structure."""
+
+    def __init__(self, data):
+        for name, value in data.items():
+            setattr(self, name, self._wrap(value))
+
+    def _wrap(self, value):
+        if isinstance(value, (tuple, list, set, frozenset)):
+            return type(value)([self._wrap(v) for v in value])
+        else:
+            return Struct(value) if isinstance(value, dict) else value
+
+
+try:
+    import ipywidgets as widgets
+
+    def get_field(field: FieldBase, input_layout: widgets.Layout):
+        if isinstance(field, Select):
+            return widgets.RadioButtons(
+                options=[(opt.label, opt.uid) for opt in field.options],
+                layout=input_layout,
+                disabled=False
+            )
+        if isinstance(field, NumberField):
+            if field.cls == int:
+                return widgets.IntSlider(layout=input_layout, min=field.min, max=field.max, step=field.step)
+
+        if field.cls == str:
+            return widgets.Text(layout=input_layout)
+        return widgets.Text(layout=input_layout)
+
+    class ConfigGeneratorUI():
+
+        def __init__(self, fields: List[FieldBase]) -> None:
+            self.fields = fields
+
+        def generate_widgets(self):
+            # TODO: Get layouts from class init
+            input_layout = widgets.Layout(width='40%')
+            label_layout = widgets.Layout(width='30%', display="flex", justify_content="flex-end")
+            desc_layout = widgets.Layout(width='30%', display="flex", justify_content="flex-start")
+            field_container_layout = widgets.Layout(
+                display='flex', width='100%', align_items='stretch', flex_flow='horiz')
+
+            def container_layout(expanded=True): return widgets.Layout(
+                display='flex',
+                border='solid 0.1px',
+                flex_flow='column',
+                align_items='stretch',
+                height='auto',
+                visibility='visible',
+            ) if expanded else widgets.Layout(
+                display='flex',
+                border='solid 0.1px',
+                flex_flow='column',
+                align_items='stretch',
+                height='0',
+                visibility='hidden',
+            )
+
+            self.field_widgets = {}
+            self.field_inputs = {}
+            for i, field in enumerate(self.fields):
+                if field.group not in self.field_widgets:
+                    self.field_widgets[field.group] = []
+                    self.field_inputs[field.group] = []
+                label = widgets.Label(field.label, layout=label_layout)
+                input_field = get_field(field, input_layout)
+                desc = widgets.Label(field.desc, layout=desc_layout)
+                contained = widgets.Box([label, input_field, desc], layout=field_container_layout)
+                self.field_widgets[field.group].append(contained)
+                self.field_inputs[field.group].append((i, input_field))
+
+            sections = []
+            for k, v in self.field_widgets.items():
+                content = widgets.Box(v, layout=container_layout(False))
+                header_btn = widgets.Button(description=k)
+                sections.append(widgets.Box([header_btn, content], layout=container_layout()))
+
+                def on_button_clicked(target, k):
+                    def inner(b):
+                        is_visable = target.layout.visibility != 'hidden'
+                        target.layout = container_layout(
+                            False) if is_visable else container_layout(True)
+                    return inner
+                header_btn.on_click(on_button_clicked(content, k))
+            boxes = widgets.VBox(sections, layout=container_layout())
+            return boxes
+
+        def get_data_dict(self):
+            out = {}
+            for group in self.field_inputs:
+                out[group] = {}
+                group_widgets = self.field_inputs[group]
+                for (i, w) in group_widgets:
+                    field = self.fields[i]
+                    out[group][field.variable] = w.value
+            return out
+
+except ImportError or ModuleNotFoundError:
+    pass
---
+++ tests/cls_construction_test.py
@@ -0,0 +1,196 @@
+import inspect
+from data_helpers.cls_parsing import dict_to_cls
+from data_helpers.encoders import AdvancedJsonDecoder, AdvancedJsonEncoder
+import pytest
+import json
+from enum import Enum
+from data_helpers.cls_construction import *
+
+
+# example_fields = [
+#     Field('main', 'foo', 'Foo', str, 'Example field', None, []),
+#     Field('main', 'bar', 'Bar', str, 'Example field', "default_bar", []),
+#     Select('main', 'sel', 'Sel', str, 'Example field', None,
+#            options=[Option('a', 'A', []), Option('b', 'B', [])]),
+# ]
+
+# example_fields = [
+#     Field('main', 'foo', 'Foo', str, 'Example field description a', True, None, []),
+#     NumberField('main', 'bar', 'Bar', int, 'Example number field', False, 9, min=3, max=33, step=3),
+#     Select('main', 'sel', 'Sel', str, 'Example field', False, None,
+#            options=[Option('a', 'A', []), Option('b', 'B', [])]),
+#     Field('other', 'bar', 'Bar', str, 'Example field', True, "default_bar", []),
+# ]
+
+
+example_fields = [
+    Field('foo', 'Foo', str, 'Example field description a', True, None, []),
+    Group('main', 'Main', [
+        NumberField('bar', 'Bar', int, 'Example number field', False, 9, min=3, max=33, step=3),
+        Select('sel', 'Sel', str, 'Example field', False, None,
+               options=[Option('a', 'A', []), Option('b', 'B', [])]),
+    ]),
+    Group('other', 'Other', [
+        Field('bar', 'Bar', str, 'Example field description a', True, "default_bar", []),
+    ]),
+]
+
+example_class = Group('GeneratedType', 'Generated Type', example_fields)
+
+
+class SelEnum(Enum):
+    A = 'a'
+    B = 'b'
+
+
+@dataclass
+class MainGroup:
+
+    sel: SelEnum
+    bar: int = 9
+
+
+@dataclass
+class OtherGroup:
+
+    bar: str = "default_bar"
+
+
+@dataclass
+class GeneratedType:
+    foo: str
+    main: MainGroup = field(default_factory=lambda: MainGroup())
+    other: OtherGroup = field(default_factory=lambda: OtherGroup())
+
+
+def check_class_match(cls_a, cls_b, verbose=False) -> bool:
+    try:
+        match_attrs = ['__class__', '__dataclass_params__', '__delattr__', '__dir__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__',
+                       '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__setattr__', '__sizeof__', '__str__', '__weakref__']
+        a_members = dict(inspect.getmembers(cls_a))
+        b_members = dict(inspect.getmembers(cls_b))
+
+        for k in a_members.keys():
+            assert k in b_members
+        for k in match_attrs:
+            # print(k)
+            assert str(a_members[k]) == str(b_members[k])
+
+        for k in a_members['__annotations__'].keys():
+            # Assert root has all groups
+            assert k in b_members['__annotations__']
+            print(f"check sub objs {k}")
+
+            a_cls = a_members['__annotations__'][k]
+            b_cls = b_members['__annotations__'][k]
+            if a_cls != b_cls:
+                a_field_members = dict(inspect.getmembers(
+                    a_cls))
+                b_field_members = dict(inspect.getmembers(
+                    b_cls))
+                print(a_cls)
+                print(b_cls)
+                print(a_field_members['__annotations__'].keys())
+                print(b_field_members['__annotations__'].keys())
+                # Assert fields in each group match
+                for f in a_field_members['__annotations__'].keys():
+                    assert str(a_field_members['__annotations__'][f]) \
+                        == str(b_field_members['__annotations__'][f])
+
+        # TODO: Check enums match
+
+        # Does not match because import location is different
+        # assert expected_members["__doc__"] == out_members["__doc__"]
+
+    except AssertionError as e:
+        if verbose:
+            raise e
+        else:
+            return False
+    return True
+
+
+class TestConstructDataclassFromDict:
+
+    def test_should_be_able_to_construct_cls_from_fields(self):
+        received, subclasses = group_to_class(
+            "GeneratedType", example_class, globals().get('__name__'))
+        assert type(received) == type(GeneratedType)
+
+        assert check_class_match(GeneratedType, received, True)
+
+    def test_should_return_subclasses(self):
+        OutCls, subclasses = group_to_class(
+            "GeneratedType", example_class, globals().get('__name__'))
+        assert type(subclasses["Main"]) == type(MainGroup)
+        assert type(subclasses["Other"]) == type(MainGroup)
+
+    def test_should_be_able_to_create_sub_objs(self):
+        OutCls, subclasses = group_to_class(
+            "GeneratedType", example_class, globals().get('__name__'))
+        MainGroupGenerated = subclasses["Main"]
+        # Note args are not type checked
+        mainGroup = MainGroupGenerated("hello", 1)
+        assert mainGroup.sel == "hello"
+        assert mainGroup.bar == 1
+
+    def test_should_be_able_to_create_instance_of_class(self):
+        OutCls, subclasses = group_to_class(
+            "GeneratedType", example_class, globals().get('__name__'))
+
+        MainGroupGenerated = subclasses["Main"]
+        mainGroup = MainGroupGenerated("hello", 1)
+        OtherGroupGenerated = subclasses["Other"]
+        otherGroup = OtherGroupGenerated("World")
+
+        assert mainGroup.sel == "hello"
+        assert otherGroup.bar == "World"
+        foo = "foo"
+        out = OutCls(foo, mainGroup, otherGroup)
+        assert out.main.sel == "hello"
+        assert out.other.bar == "World"
+
+    def test_should_correctly_raise_missing_arg_error(self):
+        OutCls, subclasses = group_to_class(
+            "GeneratedType", example_class, globals().get('__name__'))
+
+        with pytest.raises(TypeError) as e:
+            out = OutCls()
+        assert "TypeError(\"__init__() missing 3 required positional arguments: 'foo', 'main', and 'other'\")" in str(
+            e)
+
+    def test_should_parse_json_to_class_using_field_data(self):
+        pass
+
+
+# class TestStruct:
+
+#     @pytest.fixture(autouse=True)
+#     def _setup(self) -> None:
+#         self.example_dict = {
+#             "foo": "Foo",
+#             "bar": {
+#                 "a": 1,
+#                 "b": 2,
+#                 "c": 3,
+#             }
+#         }
+
+#     def test_can_create_from_dictionary(self):
+#         NewStruct = Struct(self.example_dict)
+#         assert isinstance(NewStruct, object)
+
+#     def test_stores_variable_types(self):
+#         NewStruct = Struct(self.example_dict)
+#         print(dir(NewStruct))
+#         assert NewStruct.__annotations__ == {}
+
+#     # def test_can_create_instance_of_struct(self):
+#     #     NewStruct = Struct(self.example_dict)
+#     #     struct = NewStruct()
+#     #     assert isinstance(struct, NewStruct)
+
+#     # def test_can_access_properties(self):
+#     #     NewStruct = Struct(self.example_dict)
+#     #     struct = NewStruct()
+#     #     assert struct.foo == "bar"
