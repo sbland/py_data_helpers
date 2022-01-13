852314300a810d94c9db564f62b188e3940b549d
Sam Bland <sbland.co.uk@gmail.com>
2022-01-13 17:20:57 +0000

    Update data loaders

--- data_helpers/data_loaders.py
+++ data_helpers/data_loaders.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import csv
 from .cls_parsing import dict_to_cls
 
 
@@ -8,3 +9,16 @@ def load_json_to_cls(file_path: Path, cls):
         json_data = json.load(file_raw)
         cls_obj = dict_to_cls(json_data, cls)
         return cls_obj
+
+def json_loader(fp):
+    with open(fp) as f:
+        return json.load(f)
+
+def csv_loader(fp):
+    with open(fp) as f:
+        spamreader = csv.reader(f)
+        header = next(spamreader)
+        out = []
+        for row in spamreader:
+            out.append({k:v for k, v in zip(header, row)})
+        return out
