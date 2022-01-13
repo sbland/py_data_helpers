d24f5d24e529ec336e142841931847863d17acf2
Sam Bland <sbland.co.uk@gmail.com>
2022-01-13 17:21:18 +0000

    update deploy script

--- bump_and_deploy.sh
+++ bump_and_deploy.sh
@@ -4,7 +4,12 @@ source venv/bin/activate
 if [ -z "$(git status --porcelain)" ]; then
     echo "Working directory is clean"
 else
-    git stash save -u "hold_build_deploy"
+    echo "Working directory is not clean!"
+    read -p "Stash changes and contiue(YES/no)? " stashchanges
+    if [stashchanges]; then
+        git stash save -u "hold_build_deploy"
+    else
+        echo "Working directory not clean. Commit changes or accept stashing changes." && exit 1;
 fi
 bumpversion $1
 python -m build
