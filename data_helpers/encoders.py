72d74d03e21a2071b4ad0764b40b00bafd8adbce
sam bland <sbland.co.uk@gmail.com>
2023-08-21 16:46:46 +0100

    add try catch to json encoder

--- data_helpers/encoders.py
+++ data_helpers/encoders.py
@@ -3,7 +3,8 @@ import importlib
 from dataclasses import asdict, is_dataclass
 import json
 import numpy as np
-from functools import reduce
+import warnings
+
 
 from enum import Enum
 
@@ -16,42 +17,55 @@ def is_enum(t) -> bool:
 
 class AdvancedJsonEncoder(json.JSONEncoder):
     """ Special json encoder.
+
+    Usage
+    =====
+
+    json.dumps(data, cls=AdvancedJsonEncoder, indent=4, sort_keys=True)
+
     """
 
     parse_functions = True
+    throw_errors = True
 
     def default(self, obj):
-        if getattr(obj, '__asdict__', None) and type(obj) != type:
-            # If we are parsing a type rather than an instance then we just get str(obj) instead
-            return {**obj.__asdict__(), "_parentcls": type(obj)}
-        elif isinstance(obj, list):
-            return ','.join([str(i) for i in obj])
-        elif isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif is_dataclass(obj) and type(obj) != type:
-            return asdict(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        elif isinstance(obj, np.bool_):
-            return bool(obj)
-            # TODO: Check why we did this
-            # if obj.dtype in [np.integer, np.floating, np.character, np.float64]:
-            #     return '[' + ','.join([str(i) for i in obj.tolist()]) + ']'
-            # else:
-            #     return obj.tolist()
-        elif issubclass(type(obj), Enum):
-            return obj.value
-        elif type(obj) == type:
-            return str(obj)
-        elif type(obj) == type(lambda: None):
-            # TODO: Implement this correctly
-            if self.parse_functions:
-                return "PLACEHOLDER_FUNC"
-            raise NotImplementedError(f"Cannot parse function: {obj}")
-        return json.JSONEncoder.default(self, obj)
-
+        try:
+            if getattr(obj, '__asdict__', None) and type(obj) != type:
+                # If we are parsing a type rather than an instance then we just get str(obj) instead
+                return {**obj.__asdict__(), "_parentcls": type(obj)}
+            elif isinstance(obj, list):
+                return ','.join([str(i) for i in obj])
+            elif isinstance(obj, np.integer):
+                return int(obj)
+            elif isinstance(obj, np.floating):
+                return float(obj)
+            elif is_dataclass(obj) and type(obj) != type:
+                return asdict(obj)
+            elif isinstance(obj, np.ndarray):
+                return obj.tolist()
+            elif isinstance(obj, np.bool_):
+                return bool(obj)
+                # TODO: Check why we did this
+                # if obj.dtype in [np.integer, np.floating, np.character, np.float64]:
+                #     return '[' + ','.join([str(i) for i in obj.tolist()]) + ']'
+                # else:
+                #     return obj.tolist()
+            elif issubclass(type(obj), Enum):
+                return obj.value
+            elif type(obj) == type:
+                return str(obj)
+            elif type(obj) == type(lambda: None):
+                # TODO: Implement this correctly
+                if self.parse_functions:
+                    return "PLACEHOLDER_FUNC"
+                raise NotImplementedError(f"Cannot parse function: {obj}")
+            return json.JSONEncoder.default(self, obj)
+        except Exception as e:
+            if self.throw_errors:
+                raise e
+            else:
+                warnings.warn(f"Failed to parse object: {obj}")
+                return "FAILED_TO_PARSE"
 
 class AdvancedJsonDecoder(json.JSONDecoder):
     """Special json decoder."""
