120c9ad99b59596e7538dbd4fe65fb93c1346ec4
sam bland <sbland.co.uk@gmail.com>
2023-08-21 17:09:02 +0100

    Bump version: 0.1.7 → 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="data_helpers",
-    version="0.1.7",
+    version="0.1.8",
     author="sbland",
     author_email="sblandcouk@gmail.com",
     description="A set of helpers for working with various datatypes",
