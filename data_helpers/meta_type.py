d200010fe9cedc62d55cb576f196d1bcbc3ded61
sam bland <sbland.co.uk@gmail.com>
2024-01-15 15:34:12 +0000

    Add SciFieldType

--- data_helpers/meta_type.py
+++ data_helpers/meta_type.py
@@ -10,4 +10,11 @@ class FieldType:
     description: str = None
 
 
-
+# TODO: Should implement an existing unit or science library here
+@dataclass
+class SciFieldType:
+    label: str
+    type: type
+    default: None
+    description: str = None
+    unit: str = None
